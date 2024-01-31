dacc1708794dffc2f7d399f73767ca2404be62e5
pegaislethal <140787670+pegaislethal@users.noreply.github.com>
2024-01-31 11:43:50 +0545

    Add files via upload

---
+++ weather/Rohit_Ghimire_2408291.js
@@ -0,0 +1,116 @@
+// Name - Rohit Ghimire 
+// StudentID - 2408291
+const searchBox = document.querySelector(".search_box input")
+const SearchBtn = document.querySelector(".search_box button")
+const timeZone = document.querySelector("#timezone");
+const cityName  = document.getElementById("city");
+const pressureElem = document.querySelector(".pressure")
+const spedElem = document.querySelector(".wind")  
+const weatInfoElem = document.querySelector("#weather-type")
+const tempElem  = document.querySelector(".temperature");
+const humid = document.querySelector(".humidity");
+
+async function checkWeather(city){
+    try{
+    // const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?&appid=3b5fc269d8d40df008cadde92801c72a&units=metric&q=${city}`);
+    const response = await fetch(`http://localhost/dummy/main.php?city=${city}`);
+    
+    var data = await response.json();
+    console.log("Data",data);
+    data_length = data.length;
+
+    d = new Date()
+    localTime = d.getTime()
+    localOffset = d.getTimezoneOffset() * 60000
+    utc = localTime + localOffset
+    var time = utc + (1000 * data[data_length-1].Timezone)
+    nd = new Date(time)
+
+    // Convert into 24-hour format
+    let hour = (nd.getHours());
+
+    let minutes = nd.getMinutes();
+    let weekday = nd.toLocaleString('default', { weekday: 'long' });
+    let month = nd.toLocaleString('default', { month: 'long' });
+    let date = nd.getDate();
+  
+    const timeZone = document.querySelector("#timezone");
+    timeZone.innerHTML= `${weekday} | ${month} ${date}, ${hour.toString().padStart(2,"0")}:${minutes.toString().padStart(2,"0")}`;
+
+    cityName.innerHTML = data[data_length-1].City;
+
+    tempElem.textContent = Math.round(data[data_length-1].Temperature)+" °C";
+
+   
+    humid.innerHTML = data[data_length-1].Humidity+" %";
+
+    pressureElem.innerHTML = data[data_length-1].Pressure+"hPa";
+
+    spedElem.innerHTML = data[data_length-1].Wind+" m/s";
+
+    const iconElem = document.querySelector("#imag")
+    iconElem.src = "https://github.com/pegaislethal/icon-link/blob/main/"+data[data_length-1].Icon+".png?raw=true";
+    weatInfoElem.textContent= ","+data[data_length-1].Description;
+    }
+    catch(err){
+        // alert("No city found")
+        // const timeZone = document.querySelector("#timezone");
+        // timeZone.textContent="--";
+        // cityName.innerHTML = "--";
+        // tempElem.textContent = "--"
+        // weatInfoElem.textContent = "--";
+        // humid.textContent = "--%"
+        // pressureElem.textContent = "-- hPa"
+        // spedElem.textContent = "-- m/s"
+    }
+}
+
+async function php_show(city) {
+    const response = await fetch(`http://localhost/dummy/main.php?city=${city}`);
+    var data = await response.json();
+    console.log("Data",data);    
+    data_length = data.length;
+
+    for (let i = 0; i <=6; i++) {
+        document.querySelector(`#temperature-value${i}`).textContent = "--";
+        document.querySelector(`#day${i}`).textContent ="--";
+        document.querySelector(`#humidity${i}`).textContent = "--";
+        document.querySelector(`#wind${i}`).textContent = "--";
+        document.querySelector(`#pressure${i}`).textContent = "--";
+        document.querySelector(`#icon${i}`).src = "https://github.com/pegaislethal/icon-link/blob/main/weather_notfound.jpg?raw=true";
+        document.querySelector(`#country${i}`).textContent= "--";
+        document.querySelector(`#date${i}`).textContent = "--";
+    }
+
+    for (let i = 0; i <=6; i++) {
+        document.querySelector(`#temperature-value${i}`).textContent = Math.round(data[data_length-i-1].Temperature) +"C";
+        document.querySelector(`#day${i}`).textContent = data[data_length-i-1].Days;
+        document.querySelector(`#humidity${i}`).textContent = data[data_length-i-1].Humidity+"%";
+        document.querySelector(`#wind${i}`).textContent = data[data_length-i-1].Wind + "m/s";
+        document.querySelector(`#pressure${i}`).textContent = data[data_length-i-1].Pressure +"hPa";
+        document.querySelector(`#icon${i}`).src = "https://github.com/pegaislethal/icon-link/blob/main/"+data[data_length-i-1].Icon+".png?raw=true";
+        document.querySelector(`#country${i}`).textContent= data[data_length-1].City;
+
+    }
+    
+}
+
+
+php_show('haridwar')
+checkWeather("Haridwar")
+
+
+SearchBtn.addEventListener('click',()=>{
+    
+    php_show(searchBox.value);
+    checkWeather(searchBox.value);
+})
+searchBox.addEventListener('keyup',(event)=>{
+    if (event.key=="Enter") {
+        checkWeather(searchBox.value);
+        php_show(searchBox.value);
+        
+    }
+        
+    }
+)
---
+++ weather/Rohit_Ghimire_2408291.php
@@ -0,0 +1,165 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+// create connect
+function local_host(){
+    $serverName = "localhost";
+    $userName = "root";
+    $password = "";
+    $dbname = "weather";
+
+    $conn = mysqli_connect($serverName, $userName, $password);
+
+    if (!$conn){
+        die("Connection unsuccessfull<br/>");
+    }
+
+
+    db_create($conn, $dbname);
+
+    $conn = db_connection($serverName, $userName, $password, $dbname);
+
+    table_creation($conn);
+
+    $cityName = isset($_GET['city']) ? $_GET['city']:'';
+    // $cityName = ucfirst($cityName);
+    api_fetch($conn,$cityName);
+    api_json($conn,$cityName);
+
+
+}
+function db_connection($serverName, $userName, $password, $dbname){
+
+    $conn = mysqli_connect($serverName, $userName, $password, $dbname);
+
+    // Checking if connection with database is done or not
+    if(!$conn){
+        die("Connection unsuccessfull<br/>");
+    }
+    else{
+
+        return $conn;
+    }
+}
+
+function db_create($conn, $dbname){
+    $sql_query = "CREATE DATABASE IF NOT EXISTS $dbname";
+
+    if (mysqli_query($conn, $sql_query) === FALSE){
+        echo "Database create failed<br/>";
+    }
+}
+
+function table_creation($conn){
+    $sql_query = "CREATE TABLE IF NOT EXISTS weather_info(
+        no int(11) AUTO_INCREMENT PRIMARY KEY,
+        date text,
+        city varchar(50),
+        country varchar(50),
+        day varchar(50),
+        temp float,
+        description varchar(50),
+        icon varchar(50),
+        pressure int(11),
+        humidity int(11),
+        wind float,
+        time int(50)
+    )";
+
+    if (mysqli_query($conn, $sql_query) === FALSE){
+        echo "Table create failed<br/>";
+    }
+}
+
+
+
+
+    // insert.php
+function api_fetch($conn,$cityName){
+    $api_Url = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=". $cityName ."&appid=7e4b75e1b4dee7ce1d87c0a0d59b77f0";
+    $api_data = file_get_contents($api_Url);
+    $api_data = json_decode($api_data,TRUE);
+    $temp = $api_data['main']['temp'];
+    $weathercondition = $api_data['weather'][0]['description'];
+    $humidity =  $api_data['main']['humidity'];
+    $pressure = $api_data['main']['pressure'];
+    $wind = $api_data['wind']['speed'];
+    $icon =  $api_data['weather'][0]['icon'];
+    $day = date('l');
+    $date = date("'m/d'");
+    $cityName = $api_data['name'];
+    $country = $api_data['sys']['country'];
+    $timezone = $api_data['timezone'];
+    // Insert into table
+    $sql = "SELECT Date FROM weather_info WHERE Date = $date AND City = '$cityName'";
+    $data = $conn->query($sql);
+    if ($data->num_rows === 0){
+        $sql = "INSERT INTO weather_info(Temperature,Description,Humidity,Wind,Pressure,Icon,Days,Date,City,Timezone,Country)
+        VALUES ('$temp','$weathercondition','$humidity','$wind','$pressure','$icon','$day',$date,'$cityName','$timezone','$country')";
+        if ($conn->query($sql) === FALSE){
+            echo "</br>Insertion failed";
+        }
+
+    }
+    else{
+        $sql = "UPDATE weather_info SET
+        Temperature = '$temp',
+        Description= '$weathercondition',
+        Humidity='$humidity',
+        Wind='$wind',
+        Pressure='$pressure',
+        Icon='$icon',
+        Days='$day',
+        Date=$date,
+        City='$cityName',
+        Timezone='$timezone',
+        Country='$country'
+        WHERE Date=$date AND City ='$cityName' ";
+        if ($conn->query($sql) === FALSE){
+            echo "<br>Update failed";
+        }
+    }
+    if ($conn->query($sql) === FALSE){
+        echo "<br> insertion failed";
+    }        
+}
+
+function api_json($conn,$cityName){
+    // $sql = "SELECT * FROM weather_info WHERE City = 'Haridwar' ";
+    $sql = "SELECT * FROM weather_info WHERE City = '$cityName' ";
+
+
+    
+
+    $result = $conn->query($sql);
+
+    // Check if there is any data
+    if ($result->num_rows > 0) {
+        $data = array();
+
+        // Fetch each row and add it to the data array
+        while ($row = $result->fetch_assoc()) {
+            $data[] = $row;
+        }
+
+        // Close the database connection
+        $conn->close();
+
+        // Convert data to JSON format
+        $json_data = json_encode($data);
+
+        // Set headers to indicate JSON content
+        header('Content-Type: application/json');
+
+        // Output the JSON data
+        echo $json_data;
+    } 
+    else {
+        // If no data is found, return an empty JSON array
+        echo json_encode();
+    }
+}
+
+local_host()
+
+    
+?>
